631b6d135b5f9e4d3f1b19cea95e122aa3fce8c3
Zitsuyu <yuichi09202001@yahoo.co.jp>
2022-07-16 22:12:23 +0900

    バリデーション

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+  validates :comment, presence: true
+  validates :post, presence: true
+  validates :user, presence: true
+
+
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  validates :detail, presence: true
+  validates :size_id, presence: true
+  validates :place_id, presence: true
+  validates :image, presence: true
+
+
+  belongs_to :user
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
 end
